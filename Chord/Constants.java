aa87da87fe087239e5caff08ef89280fe35318b3
Abhiman Kolte <akolte@scu.edu>
2019-03-10 14:07:01 -0700

    update

---
+++ Chord/Constants.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Constants {
+    public static HashMap<String, Integer> _refMap = new HashMap<String, Integer>() {{
+        put("Games", 1);
+        put("Movies", 2);
+        put("Cities", 3);
+        put("Nature", 4);
+        put("Car", 5);
+    }};
+
+    public static final int _mpiPort = 5678;
+}
