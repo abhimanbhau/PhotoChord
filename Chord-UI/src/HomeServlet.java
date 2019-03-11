8b8e3fec8f1037afa38f810e029eb126cc314b30
Abhiman Kolte <akolte@scu.edu>
2019-03-10 18:09:54 -0700

    Shit works

--- Chord-UI/src/HomeServlet.java
+++ Chord-UI/src/HomeServlet.java
@@ -1,10 +1,14 @@
+import com.sun.corba.se.spi.activation.Server;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
@@ -13,8 +17,26 @@ public class HomeServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
+      response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<h3>Hello World!</h3>");
+
+        Socket client = new Socket("172.21.102.189", 5678);
+        System.out.println("new client");
+        BufferedWriter os = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader( client.getInputStream()));
+        System.out.println("Setup is and os");
+        os.write("Games\r\n");
+        os.flush();
+        System.out.println("OS write bytes");
+        String result = in.readLine();
+        os.close();
+        in.close();
+        client.close();
+
+
+        out.println("<h1>Accepted input from chord</h1>");
+        out.println(result);
+        out.close();
     }
 }
--- Chord/edu/scu/thread/MessagePassingThread.java
+++ Chord/edu/scu/thread/MessagePassingThread.java
@@ -2,14 +2,15 @@ package edu.scu.thread;
 
 import edu.scu.core.Node;
 import edu.scu.util.Constants;
+import edu.scu.util.Logger;
+import sun.rmi.runtime.Log;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetAddress;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class MessagePassingThread extends Thread {
     Node node;
@@ -28,26 +29,53 @@ public class MessagePassingThread extends Thread {
 
     @Override
     public void run() {
+        DataOutputStream out = null;
         while (_run) {
             try {
                 socket = serverSocket.accept();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader reader = new BufferedReader( new InputStreamReader(socket.getInputStream()));
 
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                Logger.log("Accepted curl req");
+                String tag;
+                tag = reader.readLine();
 
-                String tag = reader.readLine();
+                out = new DataOutputStream(socket.getOutputStream());
+
+                Logger.log("accp tag: " + tag);
+
                 int tagNode = Constants.refMap.get(tag);
                 if (tagNode == node.getId()) {
                     // Send images
+
+                    ZipOutputStream zout = new ZipOutputStream(out);
+
+                    // Recurse through files
+                    for(File file : new File(Constants._photoStoragePath).listFiles()) {
+                        byte[] fileData = Files.readAllBytes(file.toPath());
+                        zout.putNextEntry(new ZipEntry(file.getName()));
+                        zout.write(fileData);
+                        zout.closeEntry();
+                    }
+                    zout.close();
+
                 } else {
                     // Send back the IP of correct node
+
+                    Logger.log("Not valid node send back correct IP");
                     out.writeBytes(Constants._nodeIpMap.get(tagNode));
                 }
+
                 out.flush();
                 out.close();
-
+                reader.close();
+                socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
+                try {
+                    out.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             }
         }
     }
